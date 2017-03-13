d321a5f8f825b3741619e0b154a0534d5c5b8f5f
bbeaulant <bo@borisbeaulant.com>
2017-03-13 08:45:07 +0100

    ~ Workflow (Topic improvements)

--- src/Ladb/CoreBundle/Topic/WorkflowTopic.php
+++ src/Ladb/CoreBundle/Topic/WorkflowTopic.php
@@ -77,12 +77,12 @@ class WorkflowTopic extends AbstractContainerAwareTopic {
 	 * @return mixed|void
 	 */
 	public function onPublish(ConnectionInterface $connection, Topic $topic, WampRequest $request, $event, array $exclude, array $eligible) {
-
-		$user = $this->_getUser($connection);
-
-		// Retieve workflow
-		$workflow = $this->_retrieveWorkflow($connection, $request, $user);
-
+//
+//		$user = $this->_getUser($connection);
+//
+//		// Retieve workflow
+//		$workflow = $this->_retrieveWorkflow($connection, $request, $user);
+//
 		$topic->broadcast($event);
 	}
 
