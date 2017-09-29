a2c234f5ca52eb6a210548a39b5f978181d9971b
bbeaulant <bo@borisbeaulant.com>
2017-09-29 09:28:17 +0200

    + Promotion

---
+++ src/Ladb/CoreBundle/Entity/Promotion/Graphic.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Ladb\CoreBundle\Entity\Promotion;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Ladb\CoreBundle\Validator\Constraints as LadbAssert;
+use Ladb\CoreBundle\Model\BodiedInterface;
+use Ladb\CoreBundle\Model\BodiedTrait;
+use Ladb\CoreBundle\Model\PicturedInterface;
+use Ladb\CoreBundle\Model\PicturedTrait;
+use Ladb\CoreBundle\Model\CommentableTrait;
+use Ladb\CoreBundle\Model\IndexableTrait;
+use Ladb\CoreBundle\Model\LikableTrait;
+use Ladb\CoreBundle\Model\ScrapableTrait;
+use Ladb\CoreBundle\Model\SitemapableInterface;
+use Ladb\CoreBundle\Model\SitemapableTrait;
+use Ladb\CoreBundle\Model\TaggableTrait;
+use Ladb\CoreBundle\Model\TitledTrait;
+use Ladb\CoreBundle\Model\ViewableTrait;
+use Ladb\CoreBundle\Model\WatchableTrait;
+use Ladb\CoreBundle\Model\IndexableInterface;
+use Ladb\CoreBundle\Model\TitledInterface;
+use Ladb\CoreBundle\Model\ViewableInterface;
+use Ladb\CoreBundle\Model\LikableInterface;
+use Ladb\CoreBundle\Model\WatchableInterface;
+use Ladb\CoreBundle\Model\CommentableInterface;
+use Ladb\CoreBundle\Model\ReportableInterface;
+use Ladb\CoreBundle\Model\TaggableInterface;
+use Ladb\CoreBundle\Model\ExplorableInterface;
+use Ladb\CoreBundle\Model\ScrapableInterface;
+use Ladb\CoreBundle\Entity\AbstractAuthoredPublication;
+
+/**
+ * @ORM\Table("tbl_promotion_graphic")
+ * @ORM\Entity(repositoryClass="Ladb\CoreBundle\Repository\Promotion\GraphicRepository")
+ * @LadbAssert\BodyBlocks()
+ */
+class Graphic extends AbstractAuthoredPublication implements TitledInterface, PicturedInterface, BodiedInterface, IndexableInterface, SitemapableInterface, TaggableInterface, ViewableInterface, ScrapableInterface, LikableInterface, WatchableInterface, CommentableInterface, ReportableInterface, ExplorableInterface {
+
+	use TitledTrait, PicturedTrait, BodiedTrait;
+	use IndexableTrait, SitemapableTrait, TaggableTrait, ViewableTrait, ScrapableTrait, LikableTrait, WatchableTrait, CommentableTrait;
+
+	const CLASS_NAME = 'LadbCoreBundle:Promotion\Graphic';
+	const TYPE = 113;
+
+	/**
+	 * @ORM\Column(type="string", length=100)
+	 * @Assert\NotBlank()
+	 * @Assert\Length(min=4)
+	 * @Assert\Regex("/^[ a-zA-Z0-9ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ'’ʼ#,.:%?!-]+$/", message="default.title.regex")
+	 */
+	private $title;
+
+	/**
+	 * @Gedmo\Slug(fields={"title"}, separator="-")
+	 * @ORM\Column(type="string", length=100, unique=true)
+	 */
+	private $slug;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="Ladb\CoreBundle\Entity\Core\Picture", cascade={"persist"})
+	 * @ORM\JoinColumn(nullable=true, name="main_picture_id")
+	 * @Assert\Type(type="Ladb\CoreBundle\Entity\Core\Picture")
+	 */
+	private $mainPicture;
+
+	/**
+	 * @ORM\Column(type="text", nullable=false)
+	 */
+	private $body;
+
+	/**
+	 * @ORM\ManyToMany(targetEntity="Ladb\CoreBundle\Entity\Core\Tag", cascade={"persist"})
+	 * @ORM\JoinTable(name="tbl_promotion_graphic_tag")
+	 * @Assert\Count(min=2)
+	 */
+	private $tags;
+
+	/**
+	 * @ORM\Column(type="integer", name="like_count")
+	 */
+	private $likeCount = 0;
+
+	/**
+	 * @ORM\Column(type="integer", name="watch_count")
+	 */
+	private $watchCount = 0;
+
+	/**
+	 * @ORM\Column(type="integer", name="comment_count")
+	 */
+	private $commentCount = 0;
+
+	/**
+	 * @ORM\Column(type="integer", name="view_count")
+	 */
+	private $viewCount = 0;
+
+	/////
+
+	public function __construct() {
+		$this->bodyBlocks = new \Doctrine\Common\Collections\ArrayCollection();
+		$this->tags = new \Doctrine\Common\Collections\ArrayCollection();
+	}
+
+	// NotificationStrategy /////
+
+	public function getNotificationStrategy() {
+		return self::NOTIFICATION_STRATEGY_FOLLOWER;
+	}
+
+	// Type /////
+
+	public function getType() {
+		return Graphic::TYPE;
+	}
+
+	// Slug /////
+
+	public function getSlug() {
+		return $this->slug;
+	}
+
+	public function setSlug($slug) {
+		$this->slug = $slug;
+		return $this;
+	}
+
+	public function getSluggedId() {
+		return $this->id.'-'.$this->slug;
+	}
+
+	// License /////
+
+	public function getLicense() {
+		return new \Ladb\CoreBundle\Entity\Core\License(true, true, true);
+	}
+
+}
