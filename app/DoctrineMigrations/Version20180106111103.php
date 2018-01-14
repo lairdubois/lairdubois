8a6b112ba0337e3c518efecfa4946cdb531ce90e
bbeaulant <bo@borisbeaulant.com>
2018-01-14 15:23:28 +0100

    ~ Doctrine migration

--- app/DoctrineMigrations/Version20180106111103.php
+++ app/DoctrineMigrations/Version20180106111103.php
@@ -18,8 +18,6 @@ class Version20180106111103 extends AbstractMigration
         $this->addSql('CREATE TABLE tbl_workflow_run (id INT AUTO_INCREMENT NOT NULL, task_id INT NOT NULL, started_at DATETIME DEFAULT NULL, finished_at DATETIME DEFAULT NULL, INDEX IDX_EE69907B8DB60186 (task_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE tbl_workflow_run ADD CONSTRAINT FK_EE69907B8DB60186 FOREIGN KEY (task_id) REFERENCES tbl_workflow_task (id)');
         $this->addSql('ALTER TABLE tbl_workflow_task DROP last_running_at');
-        $this->addSql('ALTER TABLE tbl_workflow_inspiration RENAME INDEX idx_4bcb70a82c7c2cba TO IDX_772B09CB2C7C2CBA');
-        $this->addSql('ALTER TABLE tbl_workflow_inspiration RENAME INDEX idx_4bcb70a870f2bc06 TO IDX_772B09CB70F2BC06');
     }
 
     public function down(Schema $schema)
@@ -28,8 +26,6 @@ class Version20180106111103 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE tbl_workflow_run');
-        $this->addSql('ALTER TABLE tbl_workflow_inspiration RENAME INDEX idx_772b09cb2c7c2cba TO IDX_4BCB70A82C7C2CBA');
-        $this->addSql('ALTER TABLE tbl_workflow_inspiration RENAME INDEX idx_772b09cb70f2bc06 TO IDX_4BCB70A870F2BC06');
         $this->addSql('ALTER TABLE tbl_workflow_task ADD last_running_at DATETIME DEFAULT NULL');
     }
 }
