8c27852f4ba42dbc6dfd90fe3eddd422fcd5c7ec
bbeaulant <bo@borisbeaulant.com>
2018-01-19 02:03:09 +0100

    ~ MariaDB JSON workaround

--- app/DoctrineMigrations/Version20180118093140.php
+++ app/DoctrineMigrations/Version20180118093140.php
@@ -15,7 +15,7 @@ class Version20180118093140 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE tbl_webpush_user_subscription (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, subscription_hash VARCHAR(255) NOT NULL, subscription JSON NOT NULL, INDEX IDX_FF0DFD82A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tbl_webpush_user_subscription (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, subscription_hash VARCHAR(255) NOT NULL, subscription longtext NOT NULL, INDEX IDX_FF0DFD82A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE tbl_webpush_user_subscription ADD CONSTRAINT FK_FF0DFD82A76ED395 FOREIGN KEY (user_id) REFERENCES tbl_core_user (id)');
     }
 
