c0122175a6ebc4806ce8e701c4285b86a3bc47f3
bbeaulant <bo@borisbeaulant.com>
2018-04-29 00:14:20 +0200

    + Books (improvements)

--- src/Ladb/CoreBundle/Resources/assets/js/ladb/jquery.ladb.scrollto.js
+++ src/Ladb/CoreBundle/Resources/assets/js/ladb/jquery.ladb.scrollto.js
@@ -8,7 +8,7 @@
             onAfter: null
         }, options );
 
-        var target = this;
+        var $target = this;
 
         // Ignore default behavior
         if (event != null) {
@@ -19,23 +19,27 @@
             var $win = $(window);
             var scrollPosition = $win.scrollTop();
             var visibleArea = $win.scrollTop() + $win.height();
-            var objEndPos = (target.offset().top + target.outerHeight());
+            var objEndPos = ($target.offset().top + $target.outerHeight());
             return (visibleArea >= objEndPos && scrollPosition <= objEndPos) ? true : false
         };
 
-        var targetTop = target.offset().top;
+        var targetTop = $target.offset().top;
         var scrollTop = $(window).scrollTop();
         var scrollMax = $(document).height() - $(window).height();
-        if ((scrollTop < scrollMax || targetTop - settings.offset <= scrollTop) && !inView($(target))) {
-            $(window).scrollTo(target, {
+        if ((scrollTop < scrollMax || targetTop - settings.offset <= scrollTop) && !inView($($target))) {
+            $(window).scrollTo($target, {
                 duration: settings.smoothScroll ? 500 : 0,
                 offset: { top: (targetTop - settings.offset) < scrollMax ? -settings.offset : 0 },
                 onAfter: function() {
                     if (typeof(settings.onAfter) == 'function') {
-                        settings.onAfter();
+                        settings.onAfter($target);
                     }
                 }
             });
+        } else {
+            if (typeof(settings.onAfter) == 'function') {
+                settings.onAfter($target);
+            }
         }
 
     }
