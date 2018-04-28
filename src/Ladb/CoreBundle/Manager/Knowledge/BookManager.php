387955f3fdfb78d2abcac4e404ab82a4caea30bf
bbeaulant <bo@borisbeaulant.com>
2018-04-28 19:59:23 +0200

    ~ delete book and school fix

--- src/Ladb/CoreBundle/Manager/Knowledge/BookManager.php
+++ src/Ladb/CoreBundle/Manager/Knowledge/BookManager.php
@@ -3,12 +3,20 @@
 namespace Ladb\CoreBundle\Manager\Knowledge;
 
 use Ladb\CoreBundle\Entity\Knowledge\Book;
+use Ladb\CoreBundle\Manager\Knowledge\Book\ReviewManager;
 
 class BookManager extends AbstractKnowledgeManager {
 
 	const NAME = 'ladb_core.book_manager';
 
 	public function delete(Book $book, $withWitness = true, $flush = true) {
+
+		// Delete reviews
+		$reviewManager = $this->get(ReviewManager::NAME);
+		foreach ($book->getReviews() as $review) {
+			$reviewManager->delete($review);
+		}
+
 		parent::deleteKnowledge($book, $withWitness, $flush);
 	}
 
--- src/Ladb/CoreBundle/Manager/Knowledge/SchoolManager.php
+++ src/Ladb/CoreBundle/Manager/Knowledge/SchoolManager.php
@@ -3,12 +3,20 @@
 namespace Ladb\CoreBundle\Manager\Knowledge;
 
 use Ladb\CoreBundle\Entity\Knowledge\School;
+use Ladb\CoreBundle\Manager\Knowledge\School\TestimonialManager;
 
 class SchoolManager extends AbstractKnowledgeManager {
 
 	const NAME = 'ladb_core.school_manager';
 
 	public function delete(School $school, $withWitness = true, $flush = true) {
+
+		// Delete testimonials
+		$testimonialManager = $this->get(TestimonialManager::NAME);
+		foreach ($school->getTestimonials() as $testimonial) {
+			$testimonialManager->delete($testimonial);
+		}
+
 		parent::deleteKnowledge($school, $withWitness, $flush);
 	}
 
