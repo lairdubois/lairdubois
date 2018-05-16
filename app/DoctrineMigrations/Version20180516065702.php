cc7926a13d4f434a5fb76d46fb75e6b853f6d3e9
bbeaulant <bo@borisbeaulant.com>
2018-05-16 13:36:50 +0200

    + Howto knids

--- app/DoctrineMigrations/Version20180516065702.php
+++ app/DoctrineMigrations/Version20180516065702.php
@@ -16,8 +16,6 @@ class Version20180516065702 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE tbl_howto ADD kind SMALLINT NOT NULL');
-        $this->addSql('ALTER TABLE tbl_workflow_inspiration RENAME INDEX idx_4bcb70a82c7c2cba TO IDX_772B09CB2C7C2CBA');
-        $this->addSql('ALTER TABLE tbl_workflow_inspiration RENAME INDEX idx_4bcb70a870f2bc06 TO IDX_772B09CB70F2BC06');
     }
 
     public function down(Schema $schema)
@@ -26,7 +24,5 @@ class Version20180516065702 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE tbl_howto DROP kind');
-        $this->addSql('ALTER TABLE tbl_workflow_inspiration RENAME INDEX idx_772b09cb2c7c2cba TO IDX_4BCB70A82C7C2CBA');
-        $this->addSql('ALTER TABLE tbl_workflow_inspiration RENAME INDEX idx_772b09cb70f2bc06 TO IDX_4BCB70A870F2BC06');
     }
 }
