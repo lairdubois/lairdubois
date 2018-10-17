544b0eacd19f7cb42924af16eede745d19a1f5dc
bbeaulant <bo@borisbeaulant.com>
2018-10-17 09:28:51 +0200

    ~ Switch to simple view consumer (not batch)

--- src/Ladb/CoreBundle/Consumer/ViewConsumer.php
+++ src/Ladb/CoreBundle/Consumer/ViewConsumer.php
@@ -23,6 +23,109 @@ class ViewConsumer implements ConsumerInterface, BatchConsumerInterface {
 	/////
 
 	public function execute(AMQPMessage $msg) {
+		$om = $this->container->get('doctrine')->getManager();
+		$userRepository = $om->getRepository(User::CLASS_NAME);
+		$viewRepository = $om->getRepository(View::CLASS_NAME);
+		$typableUtils = $this->container->get(TypableUtils::NAME);
+		$logger = $this->container->get('logger');
+
+		try {
+			$msgBody = unserialize($msg->getBody());
+
+			$entityType = $msgBody['entityType'];
+			$entityId = $msgBody['entityId'];
+			$userId = $msgBody['userId'];
+
+		} catch (\Exception $e) {
+			$logger->error($e);
+			return;
+		}
+
+		// Retrieve viewable
+		try {
+			$viewable = $typableUtils->findTypable($entityType, $entityId);
+		} catch(\Exception $e) {
+			$logger->error($e);
+			return;
+		}
+		$updated = false;
+
+		if (!is_null($userId)) {
+
+			$user = $userRepository->findOneById($userId);
+			if (!is_null($user)) {
+
+				// Authenticated user -> use viewManager
+
+				$view = $viewRepository->findOneByEntityTypeAndEntityIdAndUserAndKind($viewable->getType(), $viewable->getId(), $user, View::KIND_SHOWN);
+				if (is_null($view)) {
+
+					// Create a new view
+					$view = new View();
+					$view->setEntityType($viewable->getType());
+					$view->setEntityId($viewable->getId());
+					$view->setUser($user);
+					$view->setKind(View::KIND_SHOWN);
+
+					$om->persist($view);
+
+					// Exclude self contribution view
+					if ($viewable instanceof AuthoredInterface && $viewable->getUser()->getId() == $user->getId()) {
+						return;
+					}
+
+					// Increment viewCount
+					$viewable->incrementViewCount();
+
+					$updated = true;
+
+				} else {
+
+					// Exclude self contribution view
+					if ($viewable instanceof AuthoredInterface && $viewable->getUser()->getId() == $user->getId()) {
+						return;
+					}
+
+					if ($view->getCreatedAt() <= (new \DateTime())->sub(new \DateInterval('P1D'))) { // 1 day
+
+						// View is older than 1 day. Update view, increment view count.
+
+						// Reset view createAt
+						$view->setCreatedAt(new \DateTime());
+
+						// Increment viewCount
+						$viewable->incrementViewCount();
+
+						$updated = true;
+
+					}
+
+				}
+
+			}
+
+		} else {
+
+			// Increment viewCount
+			$viewable->incrementViewCount();
+
+			$updated = true;
+
+		}
+
+		if ($updated) {
+
+			// Flush DB updates (view and/or entity)
+			$om->flush();
+
+			// Update in Elasticsearch
+			if ($viewable instanceof IndexableInterface && $viewable->isIndexable()) {
+				$searchUtils = $this->container->get(SearchUtils::NAME);
+				$searchUtils->replaceEntityInIndex($viewable);
+			}
+
+		}
+
 	}
 
 	public function batchExecute(array $messages) {
