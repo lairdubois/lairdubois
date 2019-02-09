7ace0f7bc1b4eb2f12dc3097ec4c3889e004dff9
bbeaulant <bo@borisbeaulant.com>
2019-02-09 13:46:51 +0100

    ~ TypableUtils fix

--- src/Ladb/CoreBundle/Utils/TypableUtils.php
+++ src/Ladb/CoreBundle/Utils/TypableUtils.php
@@ -81,9 +81,6 @@ class TypableUtils extends AbstractContainerAwareUtils {
 			// Book
 			case \Ladb\CoreBundle\Entity\Knowledge\Book::TYPE:
 				return implode($delimiter, array( 'knowledge', 'book' ));
-			// Review (Book)
-			case \Ladb\CoreBundle\Entity\Knowledge\Book\Review::TYPE:
-				return implode($delimiter, array( 'knowledge', 'review' ));
 
 		}
 		return '';
