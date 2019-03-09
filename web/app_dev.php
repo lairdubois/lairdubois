c0420b41c66404ec63794e37dd197da225d8fbbb
bbeaulant <bo@borisbeaulant.com>
2019-03-09 14:17:43 +0100

    ~ Global Modernizr

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
+        '192.168.0.11',
+        '192.168.0.12',
         '127.0.0.1',
         '::1',
     ))
