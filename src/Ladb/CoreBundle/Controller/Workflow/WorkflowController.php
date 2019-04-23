3b9f84e2c9cfb8a70bea6bb81a7bb3a230350de8
bbeaulant <bo@borisbeaulant.com>
2019-04-23 21:45:00 +0200

    ~ Internal fix

--- src/Ladb/CoreBundle/Controller/Workflow/WorkflowController.php
+++ src/Ladb/CoreBundle/Controller/Workflow/WorkflowController.php
@@ -876,8 +876,6 @@ class WorkflowController extends AbstractWorkflowBasedController {
 	 */
 	public function internalDiagramAction($id) {
 
-		echo $_SERVER['REMOTE_ADDR'];
-
 		// Retrieve workflow
 		$workflow = $this->_retrieveWorkflow($id);
 
