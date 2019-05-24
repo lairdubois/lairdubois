273540d29630442ca5de9198715ef5519ec19527
Marc-Aurèle Brothier <m@brothier.org>
2019-05-24 07:16:10 +0200

    ~ symfony directories: move cache, vendor and logs directory outisde of the code source structure
    - `var/cache` is now at `/var/cache/ladb`
    - `var/logs` is now at `/var/log/ladb`
    - `vendor` is now at `/var/cache/ladb/vendor`

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -50,12 +50,12 @@ class AppKernel extends Kernel
 
 	public function getCacheDir()
 	{
-		return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+		return '/var/cache/ladb/'.$this->getEnvironment();
 	}
 
 	public function getLogDir()
 	{
-		return dirname(__DIR__).'/var/logs';
+		return '/var/log/ladb';
 	}
 
 	public function registerContainerConfiguration(LoaderInterface $loader)
--- app/autoload.php
+++ app/autoload.php
@@ -2,13 +2,13 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = require __DIR__.'/../vendor/autoload.php';
+$loader = require '/var/cache/ladb/vendor/autoload.php';
 
 // intl
 if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+    require_once '/var/cache/ladb/vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
+    $loader->add('', '/var/cache/ladb/vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
