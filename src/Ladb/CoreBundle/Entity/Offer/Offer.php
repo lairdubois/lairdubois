f7d06cee6d72ed9b186b9b76d221f56856c0f809
bbeaulant <bo@borisbeaulant.com>
2019-12-19 18:17:56 +0100

    + Offert entity

---
+++ src/Ladb/CoreBundle/Entity/Offer/Offer.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Ladb\CoreBundle\Entity\Find;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Ladb\CoreBundle\Validator\Constraints as LadbAssert;
+use Ladb\CoreBundle\Model\CollectionnableInterface;
+use Ladb\CoreBundle\Model\CollectionnableTrait;
+use Ladb\CoreBundle\Model\SluggedInterface;
+use Ladb\CoreBundle\Model\SluggedTrait;
+use Ladb\CoreBundle\Entity\AbstractDraftableAuthoredPublication;
+use Ladb\CoreBundle\Model\BlockBodiedInterface;
+use Ladb\CoreBundle\Model\BlockBodiedTrait;
+use Ladb\CoreBundle\Model\CommentableTrait;
+use Ladb\CoreBundle\Model\IndexableTrait;
+use Ladb\CoreBundle\Model\LikableTrait;
+use Ladb\CoreBundle\Model\PicturedTrait;
+use Ladb\CoreBundle\Model\ScrapableTrait;
+use Ladb\CoreBundle\Model\SitemapableInterface;
+use Ladb\CoreBundle\Model\SitemapableTrait;
+use Ladb\CoreBundle\Model\TaggableTrait;
+use Ladb\CoreBundle\Model\TitledTrait;
+use Ladb\CoreBundle\Model\ViewableTrait;
+use Ladb\CoreBundle\Model\WatchableTrait;
+use Ladb\CoreBundle\Model\JoinableInterface;
+use Ladb\CoreBundle\Model\ScrapableInterface;
+use Ladb\CoreBundle\Model\IndexableInterface;
+use Ladb\CoreBundle\Model\TitledInterface;
+use Ladb\CoreBundle\Model\PicturedInterface;
+use Ladb\CoreBundle\Model\ViewableInterface;
+use Ladb\CoreBundle\Model\LikableInterface;
+use Ladb\CoreBundle\Model\WatchableInterface;
+use Ladb\CoreBundle\Model\CommentableInterface;
+use Ladb\CoreBundle\Model\ReportableInterface;
+use Ladb\CoreBundle\Model\TaggableInterface;
+use Ladb\CoreBundle\Model\ExplorableInterface;
+use Ladb\CoreBundle\Entity\Find\Content\Event;
+
+/**
+ * @ORM\Table("tbl_offer")
+ * @ORM\Entity(repositoryClass="Ladb\CoreBundle\Repository\Offer\OfferRepository")
+ * @LadbAssert\BodyBlocks()
+ */
+class Offer extends AbstractDraftableAuthoredPublication implements TitledInterface, SluggedInterface, PicturedInterface, BlockBodiedInterface, IndexableInterface, SitemapableInterface, TaggableInterface, ViewableInterface, ScrapableInterface, LikableInterface, WatchableInterface, CommentableInterface, ReportableInterface {
+
+	use TitledTrait, SluggedTrait, PicturedTrait, BlockBodiedTrait;
+	use IndexableTrait, SitemapableTrait, TaggableTrait, ViewableTrait, ScrapableTrait, LikableTrait, WatchableTrait, CommentableTrait;
+
+	const CLASS_NAME = 'LadbCoreBundle:Offer\Offer';
+	const TYPE = 104;
+
+	/**
+	 * @ORM\Column(type="string", length=100)
+	 * @Assert\NotBlank()
+	 * @Assert\Length(min=4)
+	 */
+	private $title;
+
+	/**
+	 * @Gedmo\Slug(fields={"title"}, separator="-")
+	 * @ORM\Column(type="string", length=100, unique=true)
+	 */
+	private $slug;
+
+	/**
+	 * @ORM\Column(type="text", nullable=false)
+	 */
+	private $body;
+
+	/**
+	 * @ORM\Column(type="string", length=255, nullable=false)
+	 */
+	private $bodyExtract;
+
+	/**
+	 * @ORM\ManyToMany(targetEntity="Ladb\CoreBundle\Entity\Core\Block\AbstractBlock", cascade={"persist", "remove"})
+	 * @ORM\JoinTable(name="tbl_offer_body_block", inverseJoinColumns={@ORM\JoinColumn(name="block_id", referencedColumnName="id", unique=true, onDelete="cascade")})
+	 * @ORM\OrderBy({"sortIndex" = "ASC"})
+	 * @Assert\Count(min=1)
+	 */
+	private $bodyBlocks;
+
+	/**
+	 * @ORM\Column(type="integer", name="body_block_picture_count")
+	 */
+	private $bodyBlockPictureCount = 0;
+
+	/**
+	 * @ORM\Column(type="integer", name="body_block_video_count")
+	 */
+	private $bodyBlockVideoCount = 0;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="Ladb\CoreBundle\Entity\Core\Picture", cascade={"persist"})
+	 * @ORM\JoinColumn(nullable=true, name="main_picture_id")
+	 */
+	private $mainPicture;
+
+	/**
+	 * @ORM\ManyToMany(targetEntity="Ladb\CoreBundle\Entity\Core\Tag", cascade={"persist"})
+	 * @ORM\JoinTable(name="tbl_offer_tag")
+	 * @Assert\Count(min=2)
+	 */
+	private $tags;
+
+	/**
+	 * @ORM\Column(type="integer", name="like_count")
+	 */
+	private $likeCount = 0;
+
+	/**
+	 * @ORM\Column(type="integer", name="watch_count")
+	 */
+	private $watchCount = 0;
+
+	/**
+	 * @ORM\Column(type="integer", name="comment_count")
+	 */
+	private $commentCount = 0;
+
+	/**
+	 * @ORM\Column(type="integer", name="view_count")
+	 */
+	private $viewCount = 0;
+
+	/////
+
+	public function __construct() {
+		$this->bodyBlocks = new \Doctrine\Common\Collections\ArrayCollection();
+		$this->tags = new \Doctrine\Common\Collections\ArrayCollection();
+	}
+
+	/////
+
+	// NotificationStrategy /////
+
+	public function getNotificationStrategy() {
+		return self::NOTIFICATION_STRATEGY_FOLLOWER;
+	}
+
+	// Type /////
+
+	public function getType() {
+		return Find::TYPE;
+	}
+
+}
