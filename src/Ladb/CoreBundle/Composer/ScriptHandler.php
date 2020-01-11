15ce32a6afb89f449813ee0e85e93fc83ef84dba
bbeaulant <bo@borisbeaulant.com>
2020-01-11 12:28:37 +0100

    ~ Installation improvements

--- src/Ladb/CoreBundle/Composer/ScriptHandler.php
+++ src/Ladb/CoreBundle/Composer/ScriptHandler.php
@@ -12,9 +12,15 @@ class ScriptHandler {
 		$uploadsDir = 'uploads';
 		$downloadsDir = 'downloads';
 		$keysDir = 'keys';
+		$fixturesDir = 'src/Ladb/CoreBundle/Resources/fixtures';
 
 		$fs = new Filesystem();
+
+		// Create needed folders
 		$fs->mkdir(array( $uploadsDir, $downloadsDir, $keysDir ));
+
+		// Copy some fixtures
+		$fs->copy($fixturesDir.'/empty*.png', $uploadsDir);
 
 	}
 
