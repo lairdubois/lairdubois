9fe759eb5432f55eb3db6e5313f8611ce1801376
bbeaulant <bo@borisbeaulant.com>
2020-04-11 17:07:35 +0200

    ~ List improvements (try-1)

--- src/Ladb/CoreBundle/Controller/Wonder/CreationController.php
+++ src/Ladb/CoreBundle/Controller/Wonder/CreationController.php
@@ -1098,12 +1098,7 @@ class CreationController extends AbstractController {
 			},
 			function(&$filters, &$sort) use ($homepage) {
 
-				if (!$homepage || $this->get('security.authorization_checker')->isGranted('ROLE_USER') && $this->getUser()->getMeta()->getUnlistedWonderCreationCount() > 0) {
-					$sort = array('changedAt' => array('order' => 'desc'));
-				} else {
-					$filters[] = new \Elastica\Query\Range('createdAt', array( 'gte' => 'now-48h/h' ));
-					$sort = array('likeCount' => array('order' => 'desc'));
-				}
+				$sort = array('changedAt' => array('order' => 'desc'));
 
 			},
 			function(&$filters) use ($layout) {
