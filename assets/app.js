84ecdbfaecd70ce8b27816237e512346dd108a2e
GaëlB <g.bordas@boginfo.fr>
2021-10-07 09:10:36 +0200

    update webpack configuration - WIP

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-import 'jquery';
 import 'jquery-form';
 import 'jquery-ui';
 import 'jquery-ui/ui/widgets/sortable';
@@ -16,19 +15,33 @@ import 'jquery-ui/ui/disable-selection';
 import 'jquery-autocomplete';
 import 'jquery-textcomplete';
 import 'bootstrap';
-import 'bootstrap-markdown';
-import 'bootstrap-datetimepicker';
+import 'bootstrap-markdown/js/bootstrap-markdown';
+import 'eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker';
 import 'uikit';
 
-import 'js/ladb';
-
+import './js/ladb/jquery.ladb.appendscrolltotopbtn';
+import './js/ladb/jquery.ladb.counters.js'
+import './js/ladb/jquery.ladb.hrefajax.js'
+import './js/ladb/jquery.ladb.textcompletify.js'
+import './js/ladb/jquery.ladb.sharebuttonsbuilder.js'
+import './js/ladb/jquery.ladb.scrollto.js'
+import './js/ladb/jquery.ladb.appendscrolltotopbtn.js'
+import './js/ladb/jquery.ladb.autopager.js'
+import './js/ladb/jquery.ladb.maparea.js'
+import './js/ladb/jquery.ladb.smartsearch.js'
+import './js/ladb/jquery.ladb.topbartranslucent.js'
+import './js/ladb/jquery.ladb.commentwidget.js'
+import './js/ladb/jquery.ladb.votewidget.js'
+import './js/ladb/jquery.ladb.reviewwidget.js'
+import './js/ladb/jquery.ladb.feedbackwidget.js'
+import './js/ladb/jquery.ladb.boxlinkclick.js'
+import './js/ladb/ladb-common.js'
+import './js/ladb/ladb-reports.js'
+import './js/ladb/ladb-pictures.js'
+import './js/ladb/ladb-webpush-client.js'
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import 'lazyloadxt/dist/jquery.lazyloadxt.fadein.css';
-import './less/ladb/_variables.less';
-import './less/ladb/ladb.less';
-import './less/ladb/icons/ladbicons.less';
+import './styles/app.less';
 
 // start the Stimulus application
 import './bootstrap';
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
+    .addEntry('app_header', './assets/app_header.js')
     .addEntry('app', './assets/app.js')
     .addEntry('qa', './assets/qa.js')
 
