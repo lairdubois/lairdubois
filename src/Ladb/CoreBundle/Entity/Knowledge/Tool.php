807b893bccce2704d3898ff0eab3bfbaa7b30bdb
bbeaulant <bo@borisbeaulant.com>
2022-02-27 17:52:43 +0100

    + New tool family

--- src/Ladb/CoreBundle/Entity/Knowledge/Tool.php
+++ src/Ladb/CoreBundle/Entity/Knowledge/Tool.php
@@ -56,7 +56,7 @@ class Tool extends AbstractKnowledge implements ReviewableInterface {
 		Tool::FIELD_PRODUCT_NAME => array(Tool::ATTRIB_TYPE => Text::TYPE_STRIPPED_NAME, Tool::ATTRIB_MULTIPLE => true),
 		Tool::FIELD_BRAND        => array(Tool::ATTRIB_TYPE => Text::TYPE_STRIPPED_NAME, Tool::ATTRIB_MULTIPLE => false, Tool::ATTRIB_FILTER_QUERY => '@brand:"%q%"'),
 		Tool::FIELD_DESCRIPTION  => array(Tool::ATTRIB_TYPE => Longtext::TYPE_STRIPPED_NAME, Tool::ATTRIB_MULTIPLE => false),
-		Tool::FIELD_FAMILY       => array(Tool::ATTRIB_TYPE => Integer::TYPE_STRIPPED_NAME, Tool::ATTRIB_MULTIPLE => false, Tool::ATTRIB_CHOICES => array(1 => 'Outil à main', 2 => 'Outil de mesure', 3 => 'Electroportatif', 4 => 'Machine stationnaire', 6 => 'Machine semi-stationnaire', 5 => 'Gabarit'), Tool::ATTRIB_USE_CHOICES_VALUE => true, Tool::ATTRIB_FILTER_QUERY => '@family:"%q%"'),
+		Tool::FIELD_FAMILY       => array(Tool::ATTRIB_TYPE => Integer::TYPE_STRIPPED_NAME, Tool::ATTRIB_MULTIPLE => false, Tool::ATTRIB_CHOICES => array(1 => 'Outil à main', 2 => 'Outil de mesure', 3 => 'Electroportatif', 4 => 'Machine stationnaire', 6 => 'Machine semi-stationnaire', 7 => 'Outil pneumatique', 5 => 'Gabarit'), Tool::ATTRIB_USE_CHOICES_VALUE => true, Tool::ATTRIB_FILTER_QUERY => '@family:"%q%"'),
 		Tool::FIELD_POWER_SUPPLY => array(Tool::ATTRIB_TYPE => Integer::TYPE_STRIPPED_NAME, Tool::ATTRIB_MULTIPLE => false, Tool::ATTRIB_CHOICES => array(1 => 'Filaire', 2 => 'Batterie')),
 		Tool::FIELD_POWER        => array(Tool::ATTRIB_TYPE => Integer::TYPE_STRIPPED_NAME, Tool::ATTRIB_MULTIPLE => false, Tool::ATTRIB_SUFFIX => 'W'),
 		Tool::FIELD_VOLTAGE      => array(Tool::ATTRIB_TYPE => Integer::TYPE_STRIPPED_NAME, Tool::ATTRIB_MULTIPLE => false, Tool::ATTRIB_SUFFIX => 'V'),
