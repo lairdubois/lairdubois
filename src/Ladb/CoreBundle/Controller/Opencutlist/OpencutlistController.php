d2103882853d6804dc2b5e1bd97b063e5375e654
Boris Beaulant <bo@borisbeaulant.com>
2025-09-29 08:21:25 +0200

    ~ OCL dev

--- src/Ladb/CoreBundle/Controller/Opencutlist/OpencutlistController.php
+++ src/Ladb/CoreBundle/Controller/Opencutlist/OpencutlistController.php
@@ -18,7 +18,7 @@ use Ladb\CoreBundle\Utils\PaginatorUtils;
 class OpencutlistController extends AbstractController {
 
     const BRANCH_PROD = 'master';
-    const BRANCH_DEV = '7.0.1';
+    const BRANCH_DEV = '7.1.0';
 
     const DEV_LANGUAGES = [ 'fr' ];
 
